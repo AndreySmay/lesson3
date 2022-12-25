118ae07986c38eeea7cc34a62b51d6295fca8a6e
andrey <as0258@icloud.com>
2022-12-25 19:24:00 +0300

    v1

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿/*Console.Write($"Введите число ");
+int.TryParse(Console.ReadLine()!, out a);
+
+Console.WriteLine($"{a}^2 = {a * a}");
+Console.WriteLine($"{a}^2 = {a * a * a}");
+Console.WriteLine($"{a}^2 = {a * a * a * a}");
+Console.WriteLine($"{a}^2 = {a * a * a * a * a}");*/
+
+Console.Write($"Введите число ");
+int.TryParse(Console.ReadLine()!, out a); 
